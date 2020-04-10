3b0361f3ec980e1a8701ff5e31fe6357d574e210
tristanmyers <tristanmyers.developer@gmail.com>
2020-04-10 11:11:35 -0400

    figuring out how to properly do cells

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/* const cell = document.querySelector('.cell');
+
+function createCell() {
+    const main = document.querySelector('main');
+    let newCell = document.createElement(div);
+    main.appendChild(newCell);
+}
+
+for (let i = 0; i < 4; i++) {
+    createCell();
+} */
